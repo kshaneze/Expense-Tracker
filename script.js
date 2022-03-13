a55f73c357bb854dbd89c6b3d1a1d45037f04020
niksa <niksaopfor@gmail.com>
2022-03-13 17:55:28 +0100

    Adding comments

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ const submit = document.querySelector('.submit');
 const table = document.querySelector('table');
 const form = document.querySelector('form');
 
+// Adding eventListener on form
 form.addEventListener('submit', function (e) {
+  // Creating varibale so i can access table-body
   const tbodyEl = document.querySelector('tbody');
+  // This is a must - the page will reload if i dont preventDefault
   e.preventDefault();
 
+  // If these 3 fields are not empty, continue with tasks, else - submition will not be able, return
   if (
     !inputName.value == '' &&
     !inputDate.value == '' &&
     !inputAmount.value == ''
   ) {
+    // Creating table cells with values form user input.
     tbodyEl.innerHTML += `
     
   <tr class="table-row">
@@ -29,26 +34,21 @@ form.addEventListener('submit', function (e) {
   `;
   } else return;
 
-  const btnDelete = document.querySelector('.btn-table');
-  const tableRow = document.querySelector('tr');
-  const tableData = document.querySelector('.td-all');
-
-  //   btnDelete.addEventListener('click', function (e) {
-  //     e.preventDefault();
-
-  // tbodyEl.lastElementChild.remove();
-
   // delete row function
   const deleteRow = function (e) {
+    // Searching for specific classList with contains(btn-table)
     if (!e.target.classList.contains('btn-table')) {
       return;
     }
-
+    // Our click action
     const btn = e.target;
+    // closest table row to our click event will be removed/deleted
     btn.closest('tr').remove();
   };
 
+  // Calling deleteRow function on our tabel
   table.addEventListener('click', deleteRow);
 
+  // Reseting every value to empty, after subbmition
   inputName.value = inputDate.value = inputAmount.value = '';
 });
